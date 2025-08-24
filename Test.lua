4b1856b2e000a02e337bd33665e943a9406efe05
WannBot <92436592+WannBot@users.noreply.github.com>
2025-08-25 01:16:02 +0700

    Test.lua

--- Test.lua
+++ Test.lua
@@ -36,7 +36,7 @@ local function getCharHum()
     return char, char:WaitForChild("Humanoid"), char:WaitForChild("HumanoidRootPart")
 end
 
-local NAME_HINTS = {"POS ", "POS 6", "POS 3", "cp"}
+local NAME_HINTS = {"spawn", "flag"}
 
 local function getWorldPosFromInstance(inst)
     if inst:IsA("BasePart") then
